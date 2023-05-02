ee3af1696e1b5677338bea06f4b8fc9a4c29ddc4
feveja <128820930+feveja@users.noreply.github.com>
2023-05-02 19:56:59 -0400

    Creamos El Objeto Digimon

---
+++ Digimon.py
@@ -0,0 +1,16 @@
+class Digimon:
+    def __init__(self, id, nombre, stage, tipo, atributo, memoria, hp, sp, atk, df, int, spd):
+        self.id = id
+        self.nombre = nombre
+        self.stage = stage
+        self.tipo = tipo
+        self.atributo = atributo
+        self.memoria = memoria
+        self.hp = hp
+        self.sp = sp
+        self.atk = atk
+        self.df = df
+        self.int = int
+        self.spd = spd
+    def __repr__(self):
+        return f"Digimon({self.id}, {self.nombre}, {self.stage}, {self.tipo}, {self.atributo}, {self.memoria}, {self.hp}, {self.sp}, {self.atk}, {self.df}, {self.int}, {self.spd})"
